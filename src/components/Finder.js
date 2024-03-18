c7c33fe34cc25a1b9b3c768e6f43a7b5e4f77fbd
jsuastech <jsuaste86@gmail.com>
2024-03-18 17:26:42 +0100

    nueva serverless

--- src/components/Finder.js
+++ src/components/Finder.js
@@ -1,13 +1,22 @@
-import axios from 'axios';
+//import axios from 'axios';
 
 async function Finder(phrase) {
   try {
-    const response = await axios.get(`http://3.144.45.205:3000/search?phrase=${phrase}`);
+    //const response = await axios.get(`http://3.144.45.205:3000/search?phrase=${phrase}`);
     /*const response = await axios.get('https://discursosamlo.vercel.app/api/serverfinder', {
       params: {
         phrase: phrase
       }
     });*/
+    const url = `https://discursosamlo.vercel.app/api/newsearch?phrase=${encodeURIComponent(phrase)}`;
+        const response = await fetch(url, {
+          method: 'GET',
+          headers: {
+       'Content-Type': 'application/json',
+      },
+      });
+
+
     console.log(response.data);
     return response.data;
   } catch (error) {
