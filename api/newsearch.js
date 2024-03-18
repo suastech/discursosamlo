9bb5bfc761d2a58ce6f332531a5cee2e691284b3
jsuastech <jsuaste86@gmail.com>
2024-03-18 17:49:26 +0100

    tras prisma generate y quito encabezados import

--- api/newsearch.js
+++ api/newsearch.js
@@ -1,5 +1,5 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+//import { PrismaClient } from "@prisma/client";
+//const prisma = new PrismaClient();
 
 async function newsearch(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
